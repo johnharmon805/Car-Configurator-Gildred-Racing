b6c49e2c17462f2a651af680edb436ecdfa8e1bc
Andrew Lee <andrewimg@gmail.com>
2020-06-12 14:00:13 -0700

    added new colors/photos

--- car-configurator/src/App/App.jsx
+++ car-configurator/src/App/App.jsx
@@ -5,7 +5,36 @@ import Body from '../Body/Body';
 function App() {
   return (
     <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <img id="logo" src="/Assets/GR logo.png"></img>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
       <Body />
+
+
+    <div className="bottom-page">
+        <footer>
+            <p id="copyright">Copyright© Gildred Racing | Configurator Developed by John Harmon & Andrew Lee</p>
+        </footer>
+    </div>
     </div>
   );
 }
--- car-configurator/src/Body/Body.jsx
+++ car-configurator/src/Body/Body.jsx
@@ -7,9 +7,9 @@ class Body extends React.Component {
         super(props) 
         this.state = {
             isRed: true,
-            isGreen: false,
+            isLightBlue: false,
             isBlue: false,
-            isGray: false,
+            isSkyBlue: false,
             isYellow: false,
             isBlack: false,
             isWhite: false,
@@ -23,9 +23,9 @@ class Body extends React.Component {
     handleSubmit = (buttonState)=>{
         this.setState({
             isRed: false,
-            isGreen: false,
+            isLightBlue: false,
             isBlue: false,
-            isGray: false,
+            isSkyBlue: false,
             isYellow: false,
             isBlack: false,
             isWhite: false,
@@ -35,43 +35,21 @@ class Body extends React.Component {
     render() {
         return(
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <img id="logo" src="/Assets/GR logo.png"></img>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
 
 
     <div className="row">
         <div className="col-md-8">
             <div className="col">
-                <img id="car" src={this.state.isRed ? "/Assets/red.png" : this.state.isBlue ? "/Assets/Blue-Cooper.png" : null}></img>
+                <img id="car" src={this.state.isRed ? "/Assets/red.png" : this.state.isBlue ? "/Assets/Blue.png" : null}></img>
             </div>
         </div>
         <div className="col-md-4">
             <div className="row">Exterior Colors</div>
             <div className="row scrollWindow">
                 <div className="col"><button id="red" onClick={()=>this.handleSubmit("isRed")}></button></div>
-                <div className="col"><button id="green" onClick={()=>this.handleSubmit("isGreen")}></button></div>
+                <div className="col"><button id="lightBlue" onClick={()=>this.handleSubmit("isLightBlue")}></button></div>
                 <div className="col"><button id="blue" onClick={()=>this.handleSubmit("isBlue")}></button></div>
-                <div className="col"><button id="gray" onClick={()=>this.handleSubmit("isGray")}></button></div>
+                <div className="col"><button id="skyBlue" onClick={()=>this.handleSubmit("isSkyBlue")}></button></div>
                 <div className="col"><button id="yellow" onClick={()=>this.handleSubmit("isYellow")}></button></div>
                 <div className="col"><button id="black" onClick={()=>this.handleSubmit("isBlack")}></button></div>
                 <div className="col"><button id="white" onClick={()=>this.handleSubmit("isWhite")}></button></div>
@@ -79,13 +57,6 @@ class Body extends React.Component {
         </div>
     </div>
 
-
-
-    <div className="bottom-page">
-        <footer>
-            <p id="copyright">Copyright© Gildred Racing | Configurator Developed by John Harmon & Andrew Lee</p>
-        </footer>
-    </div>
         </div>
             ) 
     }
