792b7a1fa6c4a40fd8d94436ef9a6695b0ceb7e2
Andrew Lee <andrewimg@gmail.com>
2020-06-13 12:16:45 -0700

    added button images

--- car-configurator/src/Body/Body.jsx
+++ car-configurator/src/Body/Body.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './Body.css';
 
@@ -50,22 +51,22 @@ class Body extends React.Component {
     <div className="row">
         <div className="col-md-8">
             <div className="col">
-                <img id="car" src={this.state.isRed && this.state.rimOne ? "/Assets/red.png" : this.state.isRed && this.state.rimTwo ? "/Assets/red2.png" : this.state.isBlue && this.state.rimOne ? "/Assets/Blue.png" : this.state.isBlue && this.state.rimTwo ? "/Assets/Blue2.png" : this.state.isLightBlue && this.state.rimOne ? "/Assets/lightblue.png" : this.state.isLightBlue && this.state.rimTwo ? "/Assets/lightblue2.png" : this.state.isSkyBlue && this.state.rimOne ? "/Assets/Skyblue.png" : this.state.isSkyBlue && this.state.rimTwo ? "/Assets/Skyblue2.png" : this.state.isWhite && this.state.rimOne ? "/Assets/White.png" : this.state.isWhite && this.state.rimTwo ? "/Assets/white2.png" : null}></img>
+                <img id="car" alt="" src={this.state.isRed && this.state.rimOne ? "/Assets/red.png" : this.state.isRed && this.state.rimTwo ? "/Assets/red2.png" : this.state.isBlue && this.state.rimOne ? "/Assets/Blue.png" : this.state.isBlue && this.state.rimTwo ? "/Assets/Blue2.png" : this.state.isLightBlue && this.state.rimOne ? "/Assets/lightblue.png" : this.state.isLightBlue && this.state.rimTwo ? "/Assets/lightblue2.png" : this.state.isSkyBlue && this.state.rimOne ? "/Assets/Skyblue.png" : this.state.isSkyBlue && this.state.rimTwo ? "/Assets/Skyblue2.png" : this.state.isWhite && this.state.rimOne ? "/Assets/White.png" : this.state.isWhite && this.state.rimTwo ? "/Assets/white2.png" : null}></img>
             </div>
         </div>
         <div className="col-md-4">
             <div className="row">Exterior Colors</div>
             <div className="row">
-                <div className="col"><button id="red" onClick={()=>this.handleSubmit("isRed")}></button></div>
-                <div className="col"><button id="lightBlue" onClick={()=>this.handleSubmit("isLightBlue")}></button></div>
-                <div className="col"><button id="blue" onClick={()=>this.handleSubmit("isBlue")}></button></div>
-                <div className="col"><button id="skyBlue" onClick={()=>this.handleSubmit("isSkyBlue")}></button></div>
+                <div className="col"><button className="color-btns" onClick={()=>this.handleSubmit("isRed")}><img id="red-btn" src="/Assets/red-btn.png"></img></button></div>
+                <div className="col"><button className="color-btns" onClick={()=>this.handleSubmit("isLightBlue")}><img id="lightblue-btn" src="/Assets/lightblue-btn.png"></img></button></div>
+                <div className="col"><button className="color-btns" onClick={()=>this.handleSubmit("isBlue")}><img id="blue-btn" src="/Assets/blue-btn.png"></img></button></div>
+                <div className="col"><button className="color-btns" onClick={()=>this.handleSubmit("isSkyBlue")}><img id="skyblue-btn" src="/Assets/skyblue-btn.png"></img></button></div>
+                <div className="col"><button className="color-btns" onClick={()=>this.handleSubmit("isWhite")}><img id="white-btn" src="/Assets/white-btn.png"></img></button></div>
             </div>
-            <div className="row">
+            {/* <div className="row">
                 <div className="col"><button id="yellow" onClick={()=>this.handleSubmit("isYellow")}></button></div>
                 <div className="col"><button id="black" onClick={()=>this.handleSubmit("isBlack")}></button></div>
-                <div className="col"><button id="white" onClick={()=>this.handleSubmit("isWhite")}></button></div>
-            </div>
+            </div> */}
             <div className="row">Tire Rims</div>
             <div className="row">
                 <div className="col"><button id="rimOne" onClick={()=>this.handleSubmitButton("rimOne")}><img id="rim1img" src="/Assets/rim-1.png"></img></button></div>
